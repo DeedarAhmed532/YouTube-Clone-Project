a84b5de51a311d58c09184340086f850390309bd
Deedar Ahmed <deedarahmed532@gmail.com>
2023-09-17 11:22:16 +0500

    Complete from my side

---
+++ index.js
@@ -0,0 +1,26 @@
+// let Bar = document.getElementById("bars");
+
+// Bar.addEventListener("click", (e)=>{
+//     e.preventDefault();
+    
+// })
+
+const menu = document.querySelector(".sidebar");
+// const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector("#bars");
+const closeIcon= document.querySelector(".closeIcon");
+// const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    // menuIcon.style.display = "block";
+  } else{
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    // menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
